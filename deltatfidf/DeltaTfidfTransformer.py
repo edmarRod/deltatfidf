4985f735034f316bc905036318197182a8916d37
edmarRod <edmar.rodrigues1995@gmail.com>
2021-08-23 13:40:25 -0300

    Added docs + tests + separate idf

--- deltatfidf/DeltaTfidfTransformer.py
+++ deltatfidf/DeltaTfidfTransformer.py
@@ -5,10 +5,87 @@ from sklearn.preprocessing import normalize
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import _document_frequency
 from sklearn.utils.fixes import _astype_copy_false
-from sklearn.utils.validation import check_is_fitted, check_array, FLOAT_DTYPES
+from sklearn.utils.validation import check_is_fitted, FLOAT_DTYPES
 
 
 class DeltaTfidfTransformer(TransformerMixin, BaseEstimator):
+    """Transform a count matrix to a normalized tf or tf-idf representation.
+
+    Tf means term-frequency while tf-idf means term-frequency times inverse
+    document-frequency. The delta is because the difference between the tf*idf is
+    taken between the 2 classes.
+    The goal of using tf-idf instead of the raw frequencies of occurrence of a
+    token in a given document is to scale down the impact of tokens that occur
+    very frequently in a given corpus and that are hence empirically less
+    informative than features that occur in a small fraction of the training
+    corpus. The difference is used to further differentiate and identify specific
+    terms which identify the 2 classes.
+    The formula that is used to compute the tf-idf for a term t of a document d
+    in a document set is tf-idf(t, d) = tf(t, d) * idf(t), and the idf is
+    computed as idf(t) = log [ n / df(t) ] + 1 (if ``smooth_idf=False``), where
+    n is the total number of documents in the document set and df(t) is the
+    document frequency of t; the document frequency is the number of documents
+    in the document set that contain the term t. The effect of adding "1" to
+    the idf in the equation above is that terms with zero idf, i.e., terms
+    that occur in all documents in a training set, will not be entirely
+    ignored. The formula for the delta is then simply the difference between
+    the tf-idf of each class, trained on the class-specific corpsu.
+    (Note that the idf formula above differs from the standard textbook
+    notation that defines the idf as
+    idf(t) = log [ n / (df(t) + 1) ]).
+    If ``smooth_idf=True`` (the default), the constant "1" is added to the
+    numerator and denominator of the idf as if an extra document was seen
+    containing every term in the collection exactly once, which prevents
+    zero divisions: idf(t) = log [ (1 + n) / (1 + df(t)) ] + 1.
+    Furthermore, the formulas used to compute tf and idf depend
+    on parameter settings that correspond to the SMART notation used in IR
+    as follows:
+    Tf is "n" (natural) by default, "l" (logarithmic) when
+    ``sublinear_tf=True``.
+    Idf is "t" when use_idf is given, "n" (none) otherwise.
+    Normalization is "c" (cosine) when ``norm='l2'``, "n" (none)
+    when ``norm=None``.
+    Parameters
+    ----------
+    norm : {'l1', 'l2'}, default='l2'
+        Each output row will have unit norm, either:
+        - 'l2': Sum of squares of vector elements is 1. The cosine
+          similarity between two vectors is their dot product when l2 norm has
+          been applied.
+        - 'l1': Sum of absolute values of vector elements is 1.
+          See :func:`preprocessing.normalize`.
+    use_idf : bool, default=True
+        Enable inverse-document-frequency reweighting.
+    smooth_idf : bool, default=True
+        Smooth idf weights by adding one to document frequencies, as if an
+        extra document was seen containing every term in the collection
+        exactly once. Prevents zero divisions.
+    sublinear_tf : bool, default=False
+        Apply sublinear tf scaling, i.e. replace tf with 1 + log(tf).
+    Attributes
+    ----------
+    idf_ : array of shape (n_features)
+        The inverse document frequency (IDF) vector; only defined
+        if  ``use_idf`` is True.
+    n_features_in_ : int
+        Number of features seen during :term:`fit`.
+    Examples
+    --------
+    >>> from sklearn.pipeline import Pipeline
+    >>> from deltatfidf import DeltaTfidfTransformer
+    >>> from sklearn.feature_extraction.text import CountVectorizer
+    >>> import numpy as np
+    >>> corpus = ['I like cheese',"Cheese is the best", "I do not like cheese","Cheese stinks"]
+    >>> y = np.array([0,0,1,1])
+    >>> cnt_vect = CountVectorizer().fit_transform(corpus)
+    >>> X_pos = cnt_vect[y == 1]
+    >>> X_neg = cnt_vect[y == 0]
+    >>> dtf = DeltaTfidfTransformer().fit(X_pos, X_neg, y)
+    >>> print(dtf.idf_)
+    [ 0.69314718  0.         -0.69314718  0.69314718  0.         -0.69314718
+     -0.69314718  0.69314718]
+    """
+
     def __init__(self, *, norm="l2", use_idf=True, smooth_idf=True, sublinear_tf=False):
         self.norm = norm
         self.use_idf = use_idf
@@ -19,15 +96,18 @@ class DeltaTfidfTransformer(TransformerMixin, BaseEstimator):
         """Learn the idf vector (global term weights).
         Parameters
         ----------
-        X : sparse matrix of shape n_samples, n_features)
-            A matrix of term/token counts.
+        X_pos : sparse matrix of shape n_samples, n_features)
+            A matrix of term/token counts for the positive class.
+        X_neg : sparse matrix of shape n_samples, n_features)
+            A matrix of term/token counts for the negative class.
         y : None
-            This parameter is not needed to compute tf-idf.
+            This parameter is not needed to compute since the counts are passed.
         Returns
         -------
         self : object
             Fitted transformer.
         """
+
         X_pos = self._validate_data(X_pos, accept_sparse=("csr", "csc"))
         X_neg = self._validate_data(X_neg, accept_sparse=("csr", "csc"))
 
@@ -58,8 +138,11 @@ class DeltaTfidfTransformer(TransformerMixin, BaseEstimator):
             n_samples_neg += int(self.smooth_idf)
 
             # no +1 since we want values with same idf to cancel
-            frac_pos_neg = n_samples_pos / n_samples_neg
-            idf = np.log(frac_pos_neg * df_neg / df_pos)
+            # using log(x/y) == - log(y/x) so there are no zero divisions
+            self.pos_idf = -1 * np.log(df_pos / n_samples_pos)
+            self.neg_idf = -1 * np.log(df_neg / n_samples_neg)
+
+            idf = self.pos_idf - self.neg_idf
 
             self._idf_diag = sp.diags(
                 idf,
@@ -106,6 +189,28 @@ class DeltaTfidfTransformer(TransformerMixin, BaseEstimator):
             X = normalize(X, norm=self.norm, copy=False)
 
         return X
+
+    def fit_transform(self, X, X_pos, X_neg, y=None):
+        """Learn vocabulary and idf, return document-term matrix.
+        This is equivalent to fit followed by transform, but more efficiently
+        implemented.
+        Parameters
+        ----------
+        X : sparse matrix of (n_samples, n_features)
+            A matrix of term/token counts.
+        X_pos : sparse matrix of shape n_samples, n_features)
+            A matrix of term/token counts for the positive class.
+        X_neg : sparse matrix of shape n_samples, n_features)
+            A matrix of term/token counts for the negative class.
+        y : None
+            This parameter is not needed to compute since the counts are passed.
+        Returns
+        -------
+        X : sparse matrix of (n_samples, n_features)
+            Tf-idf-weighted document-term matrix.
+        """
+        self.fit(X_pos, X_neg, y)
+        return self.transform(X, copy=False)
 
     @property
     def idf_(self):
--- deltatfidf/DeltaTfidfVectorizer.py
+++ deltatfidf/DeltaTfidfVectorizer.py
@@ -3,14 +3,171 @@ from collections import Counter
 import warnings
 
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import normalize
 from sklearn.utils.validation import check_is_fitted, check_array, FLOAT_DTYPES
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 from .DeltaTfidfTransformer import DeltaTfidfTransformer
 
 
 class DeltaTfidfVectorizer(CountVectorizer):
+    r"""Convert a collection of raw documents to a matrix of TF-IDF features.
+
+    Equivalent to Sklearn CountVectorizer followed by DeltaTfidfTransformer.
+    Parameters
+    ----------
+    input : {'filename', 'file', 'content'}, default='content'
+        - If `'filename'`, the sequence passed as an argument to fit is
+          expected to be a list of filenames that need reading to fetch
+          the raw content to analyze.
+        - If `'file'`, the sequence items must have a 'read' method (file-like
+          object) that is called to fetch the bytes in memory.
+        - If `'content'`, the input is expected to be a sequence of items that
+          can be of type string or byte.
+    encoding : str, default='utf-8'
+        If bytes or files are given to analyze, this encoding is used to
+        decode.
+    decode_error : {'strict', 'ignore', 'replace'}, default='strict'
+        Instruction on what to do if a byte sequence is given to analyze that
+        contains characters not of the given `encoding`. By default, it is
+        'strict', meaning that a UnicodeDecodeError will be raised. Other
+        values are 'ignore' and 'replace'.
+    strip_accents : {'ascii', 'unicode'}, default=None
+        Remove accents and perform other character normalization
+        during the preprocessing step.
+        'ascii' is a fast method that only works on characters that have
+        an direct ASCII mapping.
+        'unicode' is a slightly slower method that works on any characters.
+        None (default) does nothing.
+        Both 'ascii' and 'unicode' use NFKD normalization from
+        :func:`unicodedata.normalize`.
+    lowercase : bool, default=True
+        Convert all characters to lowercase before tokenizing.
+    preprocessor : callable, default=None
+        Override the preprocessing (string transformation) stage while
+        preserving the tokenizing and n-grams generation steps.
+        Only applies if ``analyzer is not callable``.
+    tokenizer : callable, default=None
+        Override the string tokenization step while preserving the
+        preprocessing and n-grams generation steps.
+        Only applies if ``analyzer == 'word'``.
+    analyzer : {'word', 'char', 'char_wb'} or callable, default='word'
+        Whether the feature should be made of word or character n-grams.
+        Option 'char_wb' creates character n-grams only from text inside
+        word boundaries; n-grams at the edges of words are padded with space.
+        If a callable is passed it is used to extract the sequence of features
+        out of the raw, unprocessed input.
+    stop_words : {'english'}, list, default=None
+        If a string, it is passed to _check_stop_list and the appropriate stop
+        list is returned. 'english' is currently the only supported string
+        value.
+        There are several known issues with 'english' and you should
+        consider an alternative.
+        If a list, that list is assumed to contain stop words, all of which
+        will be removed from the resulting tokens.
+        Only applies if ``analyzer == 'word'``.
+        If None, no stop words will be used. max_df can be set to a value
+        in the range [0.7, 1.0) to automatically detect and filter stop
+        words based on intra corpus document frequency of terms.
+    token_pattern : str, default=r"(?u)\\b\\w\\w+\\b"
+        Regular expression denoting what constitutes a "token", only used
+        if ``analyzer == 'word'``. The default regexp selects tokens of 2
+        or more alphanumeric characters (punctuation is completely ignored
+        and always treated as a token separator).
+        If there is a capturing group in token_pattern then the
+        captured group content, not the entire match, becomes the token.
+        At most one capturing group is permitted.
+    ngram_range : tuple (min_n, max_n), default=(1, 1)
+        The lower and upper boundary of the range of n-values for different
+        n-grams to be extracted. All values of n such that min_n <= n <= max_n
+        will be used. For example an ``ngram_range`` of ``(1, 1)`` means only
+        unigrams, ``(1, 2)`` means unigrams and bigrams, and ``(2, 2)`` means
+        only bigrams.
+        Only applies if ``analyzer is not callable``.
+    max_df : float or int, default=1.0
+        When building the vocabulary ignore terms that have a document
+        frequency strictly higher than the given threshold (corpus-specific
+        stop words).
+        If float in range [0.0, 1.0], the parameter represents a proportion of
+        documents, integer absolute counts.
+        This parameter is ignored if vocabulary is not None.
+        This parameter is applied globally on the corpus,
+        independent of class frequency.
+    min_df : float or int, default=1
+        When building the vocabulary ignore terms that have a document
+        frequency strictly lower than the given threshold. This value is also
+        called cut-off in the literature.
+        If float in range of [0.0, 1.0], the parameter represents a proportion
+        of documents, integer absolute counts.
+        This parameter is ignored if vocabulary is not None.
+        This parameter is applied globally on the corpus,
+        independent of class frequency.
+    max_features : int, default=None
+        If not None, build a vocabulary that only consider the top
+        max_features ordered by term frequency across the corpus.
+        This parameter is ignored if vocabulary is not None.
+        This parameter is applied globally on the corpus,
+        independent of class frequency.
+    vocabulary : Mapping or iterable, default=None
+        Either a Mapping (e.g., a dict) where keys are terms and values are
+        indices in the feature matrix, or an iterable over terms. If not
+        given, a vocabulary is determined from the input documents.
+    binary : bool, default=False
+        If True, all non-zero term counts are set to 1. This does not mean
+        outputs will have only 0/1 values, only that the tf term in tf-idf
+        is binary. (Set idf and normalization to False to get 0/1 outputs).
+    dtype : dtype, default=float64
+        Type of the matrix returned by fit_transform() or transform().
+    norm : {'l1', 'l2'}, default='l2'
+        Each output row will have unit norm, either:
+        - 'l2': Sum of squares of vector elements is 1. The cosine
+          similarity between two vectors is their dot product when l2 norm has
+          been applied.
+        - 'l1': Sum of absolute values of vector elements is 1.
+    use_idf : bool, default=True
+        Enable inverse-document-frequency reweighting.
+    smooth_idf : bool, default=True
+        Smooth idf weights by adding one to document frequencies, as if an
+        extra document was seen containing every term in the collection
+        exactly once. Prevents zero divisions.
+    sublinear_tf : bool, default=False
+        Apply sublinear tf scaling, i.e. replace tf with 1 + log(tf).
+    Attributes
+    ----------
+    vocabulary_ : dict
+        A mapping of terms to feature indices.
+    fixed_vocabulary_ : bool
+        True if a fixed vocabulary of term to indices mapping
+        is provided by the user.
+    idf_ : array of shape (n_features,)
+        The inverse document frequency (IDF) vector; only defined
+        if ``use_idf`` is True.
+    stop_words_ : set
+        Terms that were ignored because they either:
+          - occurred in too many documents (`max_df`)
+          - occurred in too few documents (`min_df`)
+          - were cut off by feature selection (`max_features`).
+        This is only available if no vocabulary was given.
+    Notes
+    -----
+    The ``stop_words_`` attribute can get large and increase the model size
+    when pickling. This attribute is provided only for introspection and can
+    be safely removed using delattr or set to None before pickling.
+    Examples
+    --------
+    >>> from deltatfidf import DeltaTfidfVectorizer
+    >>> corpus = [
+    ... "I like cheese",
+    ... "Cheese is the best",
+    ... "I do not like cheese",
+    ... "Cheese stinks"
+    ... ]
+    >>> labels = [0,0,1,1]
+    >>> vectorizer = DeltaTfidfVectorizer()
+    >>> X = vectorizer.fit_transform(corpus, labels)
+    >>> vectorizer.get_feature_names()
+    ['best', 'cheese', 'do', 'is', 'like', 'not', 'stinks', 'the']
+    """
+
     def __init__(
         self,
         *,
@@ -139,7 +296,8 @@ class DeltaTfidfVectorizer(CountVectorizer):
 
         if (cnt_0 == 0) | (cnt_1 == 0):
             raise ValueError(
-                f"Expected classes to have more than 0 elements, got class 0:{cnt_0}, class 1:{cnt_1}. Check if classes are set as 0 and 1."
+                f"""Expected classes to have more than 0 elements, got class 0:{cnt_0}, class 1:{cnt_1}.
+                Check if classes are set as 0 and 1."""
             )
 
     def fit(self, raw_documents, y):
@@ -159,6 +317,7 @@ class DeltaTfidfVectorizer(CountVectorizer):
         self._check_binary(y)
         self._check_params()
         self._warn_for_unused_params()
+        y = check_array(y, ensure_2d=False)
         cnt_vect = super().fit_transform(raw_documents)
         X_pos = cnt_vect[y == 1]
         X_neg = cnt_vect[y == 0]
